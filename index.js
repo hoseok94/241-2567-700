114cd05a237f2f7a5cb0474bdfbf485810d1113e
montira naewnak <Montira.nae@ku.th>
2025-01-27 13:57:02 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const BASE_URL = 'http://localhost:8000'; 
 let mod = 'CREATE'
 let selectedId = ''
@@ -138,3 +139,30 @@ const submitData = async () => {
         messageDOM.className = 'message danger';
     }
 };
+=======
+function submitData(){
+    let firstNameDOM = document.querySelector('input[name ="firstName"]');
+    let lastNameDOM = document.querySelector('input[name ="lastName"]');
+    let ageDOM = document.querySelector('input[name ="age"]');
+    let genderDOM = document.querySelector('input[name = "gender"]');
+    let interestDOM = document.querySelectorAll('input[name = "interest"]');
+    let descriptionDOM = document.querySelector('textarea[name = "description"]');
+
+    let interest = '';
+    for (let i = 0; i < interestDOM.length; i++){
+        interest += interestDOM[i].value;
+        if (i != interestDOM.length - 1){
+            interest += ',';
+        }
+    }
+    let userData = {
+        firstName: firstNameDOM.value,
+        lastName: lastNameDOM.value,
+        age: ageDOM.value,
+        gender: genderDOM.value,
+        description: descriptionDOM.value,
+        interest: interest
+    }
+    console.log('submitData',userData);
+}
+>>>>>>> 2deefe5 (Add files via upload)
