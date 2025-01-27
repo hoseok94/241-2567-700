d0c665fa17c236786987f931b65ac9a52beb2e48
montira naewnak <Montira.nae@ku.th>
2025-01-27 13:58:01 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //string ,number ,booliean Object , array
 /*
 //string
@@ -111,3 +112,118 @@ let student = {
     grade : 'A' 
 };
     console.log(student);
+=======
+//string ,number ,booliean Object , array
+/*
+//string
+let firstname = 'John';
+const idcard = '1234';
+//number
+let age = 25;
+let height = 5.9;
+//boolean
+let isMarried = false;
+firstname = 'xxxx'; //มันจะขึ้นตัวนี้แทน เหมือนทับข้อมูลเดิม
+// let firstname = 'Johnnnnnnnn'; //มันจะเออเร่อเพราะมีอยู๋แล้ว
+console.log('my name is',firstname,'and I am',age,'years old.')
+*/
+
+/*
++ บวก
+- ลบ
+* คูณ
+/ หาร
+% เปอร์เซน หารเอาเศษ (mod)
+
+let num1 = 5;
+let num2 = 10;
+
+let result = num1 + num2;
+console.log(result)
+
+let n1 = '4';
+let n2 = '8';
+
+let result2 = n1 - n2;
+console.log('result is',result2)
+
+let n = 4;
+let nn = 8;
+let condition = n == nn; //boolean
+console.log('result of condition is ',condition)
+
+let new1 = 5;
+let new2 = 5;
+
+//if - else condition
+if (new1 >= new2){
+    console.log('this is if')
+} else if(new1 == new2) {
+    console.log('this is else if');
+} else {
+    console.log('this is else');
+}
+
+>= 80 = a
+>=70 = b
+>=60 = c
+>=50 = d
+let score = prompt('Enter your score:');
+console.log('your score is',score);
+
+if (score >= 80){
+    console.log('you are grade A')
+} else if(score >= 70) {
+    console.log('you are grade B');
+} else if(score >= 60) {
+    console.log('you are grade C');
+} else if(score >= 50) {
+    console.log('you are grade D');
+} else {
+    console.log('you are grade F');
+}
+
+*/
+
+
+
+/*
+&& และ
+|| หรือ
+! not
+*/
+// true || FALSE = true
+/*let number1 = 5;
+let number2 = 8;
+let condition = number1 >= 3 || number2 >= 10;
+console.log('result of condition',condition)
+
+
+let age = 30;
+let gender = 'male'
+if (age >= 20 && gender =='male'){
+    console.log('You are male adult');
+} */
+
+/* let number = 25;
+if (!(number % 2 == 0)){
+    console.log('you are even number');
+} */
+
+/* let counter = 0;
+while (counter<10) {
+    console.log('hi');
+    counter = counter + 1;
+}
+
+for (let counter = 0; counter < 10; counter = counter+1){
+    console.log()
+}
+*/
+let student = {
+    name : 'john',
+    age : 25,
+    grade : 'A' 
+};
+    console.log(student);
+>>>>>>> d4ce908 (Add files via upload)
